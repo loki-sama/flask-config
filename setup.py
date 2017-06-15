d2fbc1f662db63f55bebe9dc089d616d30e2c9a7
loki-sama <lucky_expert@gmx.net>
2017-06-15 15:04:01 +0200

    Create setup.py
    changed file to open as file not working for me on python3.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import re
-__src__ = file('flask_config.py').read()
+__src__ = open('flask_config.py').read()
 __doc__ = re.search('^(["\']{3})(.*?)\\1', __src__, re.M|re.S).group(2).strip()
 __version__ = re.search('^__version__\s*=\s*(["\'])(.*?)\\1\s*$', __src__, re.M).group(2).strip()
 
